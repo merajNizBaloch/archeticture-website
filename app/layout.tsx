c6e78660d5df0187163eb8c594a4ba6a1afc5f2a
Meraj Niaz Baloch <90909350+merajNizBaloch@users.noreply.github.com>
2026-09-15 01:36:45 +0500

    Load working brand styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./interaction.css";
 import "./transitions.css";
 import "./showcase.css";
 import "./v3.css";
+import "./brand.css";
 import SmoothScroll from "@/components/SmoothScroll";
 import ArchitectureCursor from "@/components/ArchitectureCursor";
 import ProjectMotion from "@/components/ProjectMotion";
